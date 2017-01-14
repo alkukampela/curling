98915d57738052a15eae907d4b7fc778aab6bbac
Juuso Meriläinen <juuso.merilainen@gofore.com>
2017-01-14 15:35:12 +0200

    physics: remove stones that are out of bounds, stop when all the stones have stopped
    TODO: actually sensible values for these

--- physics/src/simulation.js
+++ physics/src/simulation.js
@@ -1,11 +1,20 @@
 import Matter from 'matter-js'
-const { Engine, Render, World, Bodies, Body, Events, Vector } = Matter;
+const { Engine, Render, World, Bodies, Body, Events, Vector, Bounds } = Matter
 
 const FRICTION = 0.1
-const RADIUS = 35
+const RADIUS = 5
+
+// Stones out of bounds are removed from the sheet.
+// The simulation is stopped when there are either no stones
+// inside bounds or all the stones are slower than MIN_SPEED.
+const BOUNDS = {
+  min: { x: 0, y: 0 },
+  max: { x: 800, y: 600 },
+}
+const MIN_SPEED = 0.001
 
 // TODO mathematics
-const getVelocity = (speed, angle) => Vector.create(0, 0)
+const getVelocity = (speed, angle) => Vector.create(0, 10)
 
 const createStone = (x, y, team) => {
   const stone = Bodies.circle(x, y, RADIUS, { frictionAir: FRICTION })
@@ -14,10 +23,17 @@ const createStone = (x, y, team) => {
 }
 
 const createStones = (delivery, stones) => {
-  const stationary = stones.map(stone => createStone(stone.x, stone.y, stone.team))
+  const stationary = stones.map(s => createStone(s.x, s.y, s.team))
   const delivered = createStone(delivery.start_x, 0, delivery.team)
   Body.setVelocity(delivered, getVelocity(delivery.speed, delivery.angle))
   return [delivered, ...stationary]
+}
+
+const createEngine = matterStones => {
+  const engine = Engine.create()
+  engine.world.gravity = Vector.create(0, 0)
+  World.add(engine.world, matterStones)
+  return engine
 }
 
 const stoneToJson = stone => {
@@ -26,27 +42,31 @@ const stoneToJson = stone => {
   return { x, y, team }
 }
 
-const createEngine = () => {
-  const engine = Engine.create()
-  engine.world.gravity = Vector.create(0, 0)
-  return engine
-}
+const isOutOfBounds = stone => !Bounds.overlaps(stone.bounds, BOUNDS)
+const isMoving = stone => Vector.magnitude(stone.velocity) > MIN_SPEED
 
-const render = () => {
+const render = (delivery, stones) => {
   // TODO frontend rendering
 }
 
 const simulate = (delivery, stones) => {
   const matterStones = createStones(delivery, stones)
-  const engine = createEngine()
-  World.add(engine.world, matterStones)
-  // TODO run until all the stones are outside field or have stopped
-  for (let i = 0; i < 10000; i++) {
-    Matter.Events.trigger(engine, 'tick', { timestamp: engine.timing.timestamp });
-    Matter.Engine.update(engine, engine.timing.delta);
-    Matter.Events.trigger(engine, 'afterTick', { timestamp: engine.timing.timestamp });
+  const engine = createEngine(matterStones)
+  const world = engine.world
+
+  while (world.bodies.length > 0 && world.bodies.some(isMoving)) {
+    // Update matter simulation
+    Events.trigger(engine, 'tick', { timestamp: engine.timing.timestamp })
+    Engine.update(engine, engine.timing.delta)
+    Events.trigger(engine, 'afterTick', { timestamp: engine.timing.timestamp })
+
+    // Remove items that are out of bounds
+    world.bodies
+      .filter(isOutOfBounds)
+      .forEach(stone => World.remove(world, stone))
   }
-  return matterStones.map(stoneToJson)
+
+  return world.bodies.map(stoneToJson)
 }
 
 export { simulate, render }
