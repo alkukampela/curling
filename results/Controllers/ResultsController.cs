6ff042eb7fe735520807188c4a785bcdbcdcd1b0
Joni Nevalainen <joni.nevalainen@gofore.com>
2017-01-14 13:21:03 +0200

    initial implementation for api/results endpoint by fetching some random data from the internet

--- results/Controllers/ResultsController.cs
+++ results/Controllers/ResultsController.cs
@@ -1,18 +1,89 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// api/results
+// [
+//     {
+//         "game_id": "492dda052473",
+//         "team_1": "Sorsa-Sepot",
+//         "team_2": "Paha-Kalevit"
+//     },
+//     {
+//         "game_id": "a90ddba4b735",
+//         "team_1": "Hermorauniot",
+//         "team_2": "Luu-5"
+//     },
+//     {
+//         "game_id": "810c97fa295c",
+//         "team_1": "Jääkenttien Kuninkaat",
+//         "team_2": "Luudankylväjät"
+//     }
+// ]
 
 namespace results.Controllers
 {
+    // Dummy class for random test api
+    class UserModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string username { get; set; }
+    }
+
+    class GameModel
+    {
+        public string Game_id { get; set; }
+        public string Team_1 { get; set; }
+
+        public string Team_2 { get; set; }
+    }
+
     [Route("api/[controller]")]
     public class ResultsController : Controller
     {
+        const string API_ENDPOINT = "https://jsonplaceholder.typicode.com";
+
         [HttpGet]
-        public string Get()
+        public async Task<IActionResult> Get()
         {
-            return "Sorry, no results";
+            var games = new List<GameModel>();
+
+            foreach (UserModel user in await this.FetchGames())
+            {
+                var game = new GameModel()
+                {
+                    Game_id = user.id.ToString(),
+                    Team_1 = user.name,
+                    Team_2 = user.username
+                };
+                games.Add(game);
+            }
+
+            return Json(games);
+        }
+
+        private async Task<IList<UserModel>> FetchGames()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(API_ENDPOINT);
+                    var response = await client.GetAsync("/users");
+                    var stringResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<UserModel>>(stringResponse);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e);
+                    return new List<UserModel>();
+                }
+            }
         }
 
         [HttpGet("{gameId}")]
