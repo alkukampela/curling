e8891bb281223152e41a34f38443cb59c0d79c8a
Risto Kuusterä <risto.kuustera@gofore.com>
2017-01-14 14:13:31 +0200

    Added check for team in delivery turn.

--- gamemanager/app.py
+++ gamemanager/app.py
@@ -21,8 +21,10 @@ PROP_GAME_ID = 'game_id'
 PROP_TEAMS = 'teams'
 PROP_STONES_IN_END = 'stones_in_end'
 PROP_TOTAL_ENDS = 'total_ends'
-PROP_STONES_THROWN = 'stones_thrown'
+PROP_STONES_DELIVERED = 'stones_delivered'
+PROP_DELIVERY_TURN = 'delivery_turn'
 PROP_END_SCORES = 'end_scores'
+PROP_TEAM_WITH_HAMMER = 'team_with_hammer'
 PROP_TEAM = 'team'
 
 @app.route('/devapi/create')
@@ -30,7 +32,7 @@ def create_game():
     game = get_new_game(generate_new_id())
 
     team_red_jwt = generate_jwt(game[PROP_GAME_ID], RED_TEAM)
-    team_yellow_jwt = generate_jwt(game[PROP_GAME_ID], YELLOW_TEAM)    
+    team_yellow_jwt = generate_jwt(game[PROP_GAME_ID], YELLOW_TEAM)
     
     resp = str(json.dumps(game)) + '\n\n\n'
     resp += str(team_red_jwt) + '\n'
@@ -43,16 +45,20 @@ def create_game():
 
 
 
-@app.route('/api/get_status/<jwt_token>')
-def status_for_delivery(jwt_token):
+@app.route('/api/get_game_status/<jwt_token>')
+def get_game_status(jwt_token):
     try:
         response_data = jwt.decode(jwt_token, SECRET, algorithms=['HS256'])
     except:
-        return Response(status = 400)
+        return Response(status=400)
 
+    # TODO: call data service
+    game = get_new_game(response_data[PROP_GAME_ID])
 
+    # Tarkasta heittovuoro
+    if response_data[PROP_TEAM] != game[PROP_DELIVERY_TURN]:
+        return Response(status=420)
     
-
 
     return Response(
         response=json.dumps(response_data),
@@ -67,32 +73,27 @@ def get_new_game(game_id):
     game = {}
     game[PROP_GAME_ID] = game_id
     
-    teams = {}
-    teams[RED_TEAM] = 'Sorsa-Sepot'
-    teams[YELLOW_TEAM] = 'Paha-Kalevit'
-    
+    teams = {
+        RED_TEAM: 'Sorsa-Sepot',
+        YELLOW_TEAM: 'Paha-Kalevit'
+    }
     game[PROP_TEAMS] = teams
     game[PROP_STONES_IN_END] = STONES_IN_END
     game[PROP_TOTAL_ENDS] = TOTAL_ENDS
 
-    stones_thrown = {}
-    stones_thrown[RED_TEAM] = 0
-    stones_thrown[YELLOW_TEAM] = 0
-    
-    game[PROP_STONES_THROWN] = stones_thrown
+    game[PROP_DELIVERY_TURN] = RED_TEAM
+    # Red always starts so yellow has a hammer
+    game[PROP_TEAM_WITH_HAMMER] = YELLOW_TEAM
+
+    stones_delivered = {
+        RED_TEAM: 0,
+        YELLOW_TEAM: 0
+    }
+
+    game[PROP_STONES_DELIVERED] = stones_delivered
 
     game[PROP_END_SCORES] = []
     return game
-
-
-
-
-
-
-
-
-
-
 
 
 
