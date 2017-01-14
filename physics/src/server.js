bd08468986de7ff085a6453fedc03db267eafb49
Juuso Meriläinen <juuso.merilainen@gofore.com>
2017-01-14 14:00:09 +0200

    Add physics simulation to root level docker-compose.yml

--- physics/src/server.js
+++ physics/src/server.js
@@ -5,7 +5,7 @@ import simulate from './simulation'
 const server = express();
 server.use(bodyParser.json());
 
-server.post('/simulate', (req, res) => {
+server.post('/', (req, res) => {
   const { delivery, stones } = req.body
   const results = simulate(delivery, stones)
   res.status(200).json(results)
