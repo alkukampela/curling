0415b4bf3c581442cd8237b9699956f30c22397d
Risto Kuusterä <risto.kuustera@gofore.com>
2017-01-14 20:08:49 +0200

    Stone delivery stupid implementation

--- delivery/app/index.js
+++ delivery/app/index.js
@@ -6,6 +6,7 @@ let app = express();
 
 app.use(morgan('combined'));
 
+/*
 app.get('/*', function (req, res) {
   let dataUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
 
@@ -18,5 +19,26 @@ app.get('/*', function (req, res) {
     console.error(error);
   });
 })
+*/
+
+app.put('/deliver_stone', function (req, res) {
+  let jwt = req.headers.authorization.split(' ')[1]
+  let dataUrl = 'http://gateway/gamemanager/get_game_status/'+jwt;
+
+  axios.get(dataUrl)
+  .then(function (response) {
+    res.status(200).json(response.data);
+  })
+  .catch(function (error) {
+    res.status(500).json({});
+    console.error(error);
+  });
+
+})
+
+
+
+
+
 
 module.exports = app;
--- delivery/index.js
+++ delivery/index.js
@@ -1,5 +1,4 @@
 let express = require('express');
-let request = require('request');
 let app = require('./app');
 
 app.listen(3000, function () {
@@ -7,16 +6,6 @@ app.listen(3000, function () {
 })
 
 
-app.put('/deliver_stone', function (req, res) {
-  let jwt = req.headers.authorization.split(' ')[1]
-
-  request('http://localhost:9600/get_game_status/'+jwt, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body)
-  } else {
-    console.log(error);
-  }
-})
 
 
 })
