b02d32bc12cbb42ce419901b43b0fe082c44fff5
Risto Kuusterä <risto.kuustera@gofore.com>
2017-01-14 20:26:54 +0200

    rrrrreally dirty fix for get_game_status

--- gamemanager/app.py
+++ gamemanager/app.py
@@ -61,8 +61,11 @@ def get_game_status(jwt_token):
     if response_data[PROP_TEAM] != game[PROP_DELIVERY_TURN]:
         return Response(status=420)
     
-    response_data[PROP_LAST_STONE] = check_for_last_stone(
-        game[PROP_STONES_DELIVERED], game[PROP_STONES_IN_END])
+    response_data[PROP_LAST_STONE] = False
+    
+    
+    #check_for_last_stone(
+    #    game[PROP_STONES_DELIVERED], game[PROP_STONES_IN_END])
 
     return Response(
         response=json.dumps(response_data),
@@ -183,7 +186,7 @@ def generate_new_id():
 def get_game_from_dataservice(game_id):
     response = requests.get(f'http://gateway/data-service/games/{game_id}')
     print('Hoo: '+str(response.status_code))
-    return json.loads(response.json())
+    return response.json()
 
 def create_game_in_dataservice(game_id, game):
     response = requests.post(f'http://gateway/data-service/games/{game_id}', data = game)
