5281126df5939504db98402fb3e79b33430eadf7
Janne Ylinen <janne.ylinen@gmail.com>
2017-01-14 20:22:51 +0200

    fix delivery

--- delivery/app/index.js
+++ delivery/app/index.js
@@ -6,33 +6,32 @@ let app = express();
 
 app.use(morgan('combined'));
 
-/*
-app.get('/*', function (req, res) {
-  let dataUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-
-  axios.get(dataUrl)
-  .then(function (response) {
-    res.status(200).json(response.data);
-  })
-  .catch(function (error) {
-    res.status(500).json({});
-    console.error(error);
-  });
-})
-*/
-
 app.put('/deliver_stone', function (req, res) {
-  let jwt = req.headers.authorization.split(' ')[1]
+  var authorization = req.headers.authorization;
+
+  if(!authorization){
+    return res.status(401).json({});
+  }
+
+  let jwt = authorization.split(' ')[1]
   let dataUrl = 'http://gateway/gamemanager/get_game_status/'+jwt;
 
   axios.get(dataUrl)
-  .then(function (response) {
-    res.status(200).json(response.data);
-  })
-  .catch(function (error) {
-    res.status(500).json({});
-    console.error(error);
-  });
+    .then(function (response) {
+      if(resonse.status === 200) {
+        console.log("ok!");
+
+
+        res.status(200).json(response.data);
+      }
+      else {
+        res.status(500).json({});
+      }
+    })
+    .catch(function (error) {
+      res.status(500).json({});
+      console.error(error);
+    });
 
 })
 
--- delivery/index.js
+++ delivery/index.js
@@ -3,9 +3,4 @@ let app = require('./app');
 
 app.listen(3000, function () {
   console.log('Delivery service listening on port 3000!')
-})
-
-
-
-
-})
+});
