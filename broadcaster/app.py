956a8a002376c2f0e67daf8a6c968638b704210f
Risto Kuusterä <risto.kuustera@gofore.com>
2017-01-15 19:19:28 +0200

    Added cors allowance for broadcaster, cleaned up api and fixed docker-configs

---
+++ broadcaster/app.py
@@ -0,0 +1,26 @@
+import json
+
+from flask import Flask, request, Response
+from flask_socketio import SocketIO, join_room, send
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.debug = True
+cors = CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app)
+
+
+@app.route('/<game_id>', methods=['POST'])
+def publish(game_id):
+    socketio.emit('new_delivery', request.json, room=game_id)
+    return Response(status=200)
+
+
+@socketio.on('subscribe')
+def on_subscribe(message):
+    game_id = message['game_id']
+    join_room(game_id)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0')
