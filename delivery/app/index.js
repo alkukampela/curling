4fec014d94d868bfc2b32177f71fde5693ec125b
Risto Kuusterä <risto.kuustera@gofore.com>
2017-01-18 18:40:57 +0200

    Some work on delivery, not there yet but simulation and broadcasting is called

--- delivery/app/index.js
+++ delivery/app/index.js
@@ -15,6 +15,9 @@ const simulator = 'http://gateway/physics/'
 
 function getGame(jwtToken) {
   console.log("getGame", jwtToken);
+
+  // TODO: check if response is not 200 and then return it
+  // i.e game not found or other player's turn
   return axios.get(gameManager + 'game_status/' + jwtToken);
 }
 
@@ -54,7 +57,7 @@ function validateRequest(req, res) {
   }
 
   if(!validateDeliveryParams(req.query)) {
-    return res.status(400).json({});
+    return res.status(400).json({"error": "Wrong params"});
   }
 
   // Authorization header format is: 'Bearer <token>'
@@ -69,9 +72,10 @@ function getSimulationParams(game, deliveryParams) {
           team: game.team,
           speed: deliveryParams.speed,
           angle: deliveryParams.angle,
-          start_x: deliveryParams.start
+          start_x: deliveryParams.start_x
         },
-        stones: stoneResponse.data
+        // TODO: this should not be necessary, stone response should always be array
+        stones: R.isEmpty(stoneResponse.data) ? [] : stoneResponse.data
       };
 
       return Promise.resolve(simulationParams);
@@ -128,9 +132,12 @@ app.put('/deliver_stone', function(req, res) {
 
   getGame(jwt)
     .then(gameResponse => {
+      console.log('GAME_RESPONSE: ' + gameResponse.data);
+
       if(gameResponse.status !== 200) {
         return res.status(gameResponse.status).json(gameResponse.data);
       }
+
 
       return getSimulationParams(gameResponse.data, req.query)
         .then(simulationParams => makeDelivery(gameResponse.data.game_id, simulationParams))
