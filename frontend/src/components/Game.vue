82631dfd8097b777ac11a8693d1c1a54995ad4cc
Janne Ylinen <janne.ylinen@gmail.com>
2017-01-31 17:13:56 +0200

    Add score board rendering

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -7,41 +7,31 @@
         :stoneCount="activeGame.stones_in_end"
         :deliveredCount="activeGame.stones_delivered.team_1"
       />
+
       <div class="stats">
         <table cellpadding="0" cellspacing="0">
           <thead>
             <tr>
               <!-- <th class="played">3</th>
               <th class="active">4</th> -->
-              <th v-for="n in 10" v-bind:class="{ active: false}">
+              <th v-for="n in maxEndCount" v-bind:class="{ active: n === activeEnd, played: n < activeEnd}">
                 <span v-if="n <= activeGame.total_ends">{{ n }}</span>
               </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>0</td>
-              <td class="team_1">3</td>
-              <td>0</td>
-              <td class="active">H</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <!-- <td class="team_1">3</td> -->
+              <td v-for="n in maxEndCount" v-bind:class="{ active: activeEnd === n, team_1: isEndWinner(n, 'team_1')}">
+                <span v-if="n < activeEnd">{{ getEndScore(n).team_1 }}</span>
+              </td>
             </tr>
             <tr>
-              <td>0</td>
-              <td>0</td>
-              <td class="team_2">2</td>
-              <td class="active"></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <!-- <td class="team_2">2</td>
+              <td class="active"></td> -->
+              <td v-for="n in maxEndCount"  v-bind:class="{ active: activeEnd === n, team_2: isEndWinner(n, 'team_2')}">
+                <span v-if="n < activeEnd">{{ getEndScore(n).team_2 }}</span>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -56,12 +46,14 @@
           </div>
         </div>
       </div>
+
       <team
         slug="team_2"
         :name="activeGame.teams.team_2"
         :stoneCount="activeGame.stones_in_end"
-        :deliveredCount="activeGame.stones_delivered.team_1"
+        :deliveredCount="activeGame.stones_delivered.team_2"
       />
+
     </header>
     <content>
       <sheet :activeGameId="activeGameId" />
@@ -70,6 +62,9 @@
 </template>
 
 <script>
+
+import R from 'ramda';
+
 import Sheet from './Sheet.vue';
 import Team from './Team.vue';
 
@@ -77,6 +72,7 @@ export default {
   props: ['activeGameId'],
   data () {
     return {
+      maxEndCount: 10,
       activeGame: {
         "stones_in_end": 0,
         "total_ends": 0,
@@ -98,6 +94,11 @@ export default {
       }
     }
   },
+  computed: {
+    activeEnd() {
+      return this.activeGame.end_scores.length + 1;
+    }
+  },
   methods: {
     getGame(gameId) {
       this.$http.get('http://localhost/results/' + gameId).then(response => {
@@ -109,6 +110,21 @@ export default {
     },
     hasTeamTurn(teamName){
       return teamName === this.activeGame.delivery_turn;
+    },
+    getEndScore(endNumber) {
+      return this.activeGame.end_scores[endNumber-1];
+    },
+    sortEndScore(end) {
+      return R.sort((a, b) => { a[1] + b[1] }, R.toPairs(end));
+    },
+    isEndWinner(endNumber, teamName) {
+      let endScore = this.getEndScore(endNumber);
+
+      if(!endScore) {
+        return false;
+      }
+
+      return teamName === R.head(this.sortEndScore(endScore))[0];
     }
   },
   mounted() {
